0390f0f2c3ec98360fce0e22384b7653a791d694
LitanSarker <litanpstucse@gmail.com>
2024-04-18 19:47:21 -0400

    Add project files.

---
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //one to many relationship
+        [JsonIgnore]
+        public List<Employee> Employees { get; set; }
+
+
+    }
+}
